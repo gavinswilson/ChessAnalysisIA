763aa2818cb7d7df587e9520e1d98f92135cec0c
Gavin Wilson <gavinswilson@gmail.com>
2022-11-15 22:25:39 -0800

    calculated aggregate distances

--- load game.py
+++ load game.py
@@ -1,16 +1,24 @@
 import chess
 import chess.pgn
-  
-# creating a virtual chessboard
+import math
+from tabulate import tabulate
+
+def calcDist(startloc, endloc):
+    # x1 = startloc%8 + 1
+    # y1 = startloc//8 + 1
+    # x2 = endloc%8 + 1
+    # y2 = endloc//8 + 1
+    p = [startloc%8 + 1, startloc//8 + 1]
+    q = [endloc%8 + 1, endloc//8 + 1]
+
+    return round(math.dist(p, q),2)
+
 board = chess.Board()
-  
-print(board)
 
 pgn = open("testgame.pgn")  # opening the file in python
-
 game = chess.pgn.read_game(pgn)  # reading the game present in file
 
-print(game)
+#print(game)
 # username of the player playing with white
 white_username = game.headers['White']
   
@@ -32,22 +40,44 @@ print("Game Result:", game_result)
 # If white wins: 1-0
 # If black wins: 0-1
 # If game drawn: 1/2-1/2
-
-# The move number for which we want the FEN
-move_number = 8
-  
+moveNum = 0
+gameMoveData = {
+    "P":0,
+    "R":0,
+    "N":0,
+    "B":0,
+    "Q":0,
+    "K":0,
+    "p":0,
+    "r":0,
+    "n":0,
+    "b":0,
+    "q":0,
+    "k":0,
+}
 # Go through each move in the game until
 # we reach the required move number
 for number, move in enumerate(game.mainline_moves()):
       
     # It copies the move played by each 
     # player on the virtual board
+    # print(board)
     board.push(move)
-      
+    curPiece = str(board.piece_at(move.to_square))
+    if (moveNum % 2) == 0:
+        color = "white"
+    else:
+        color = "black"
+    # print(color+"\t"+ move, move.from_square, move.to_square, calcDist(move.from_square, move.to_square), board.piece_at(move.to_square))
+    print(color+"\t"+str(move)+"\t"+str(calcDist(move.from_square, move.to_square))+"\t"+str(board.piece_at(move.to_square)))
+    curDist = gameMoveData[curPiece] + calcDist(move.from_square, move.to_square)
+    gameMoveData.update({curPiece: round(curDist,2)})
+    moveNum += 1
     # Remember that number starts from 0
-    if number == move_number:  
+    if game.is_end():
         break
-  
-fen = board.fen()
-print(fen)
+print(gameMoveData)
 print(board)
+# fen = board.fen()
+# print(fen)
+# print(board)
